55c7047f582264137a73d232cf0f4cec980a125a
Melike Sinem Kaya <msinemkaya6@gmail.com>
2023-05-05 19:11:26 +0300

    feat: created the list items

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,19 @@
-export default function ExpenseItem(){
+import {MdEdit, MdDelete} from 'react-icons/md'
+
+export default function ExpenseItem({ expense }){
+  const {id, charge, amount} = expense
   return(
     <>
-      
+      <li className='item'>
+        <div className='info'>
+          <span className='expense'>{charge}</span>
+          <span className="amount">${amount}</span>
+        </div>
+        <div className=''>
+          <button className='edit-btn'><MdEdit /></button>
+          <button className='clear-btn'><MdDelete /></button>
+        </div>
+      </li>
     </>
   );
 }
