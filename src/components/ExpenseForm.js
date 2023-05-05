9ff5350844a113f27b43a323f6165c2988a93a4c
Melike Sinem Kaya <msinemkaya6@gmail.com>
2023-05-05 19:24:12 +0300

    feat: created the form area for inputs

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,7 +1,22 @@
-export default function ExpenseForm(){
-  return(
-    <>
-      
-    </>
+import { MdSend } from 'react-icons/md';
+
+export default function ExpenseForm() {
+  return (
+    <form>
+      <div className='form-center'>
+        <div className='form-group'>
+          <label htmlFor='charge'>charge</label>
+          <input type='text' className='form-control' id='charge' name='charge' placeholder='e.g. rent'/>
+        </div>
+        <div className='form-group'>
+          <label htmlFor='amount'>amount</label>
+          <input type='text' className='form-control' id='amount' name='amount' placeholder='e.g. 600'/>
+        </div>
+      </div>
+      <button type='submit' className='btn'>
+        submit
+        <MdSend className='btn-icon' />
+      </button>
+    </form>
   );
 }
