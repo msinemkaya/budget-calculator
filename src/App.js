39ac33ea26bfb1b74647068a42972a8fe516b218
Melike Sinem Kaya <msinemkaya6@gmail.com>
2023-05-05 17:15:42 +0300

    feat: built the basic structure of the app component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import './App.css'
+import ExpenseForm from './components/ExpenseForm'
+import ExpenseList from './components/ExpenseList'
+import Alert from './components/Alert'
+import uuid from 'uuid'
+import { useState } from 'react'
 
 function App() {
+
+  const [expenses, setExpenses] = useState([])
+
   return (
     <>
-
+      <Alert />
+      <h1>budget calculator</h1>
+      <main className='App'>
+        <ExpenseForm/>
+        <ExpenseList expenses={expenses}/>
+      </main>
+      <h1>
+        Total Spending: <span className='total'>${expenses.reduce((acc, curr) => (
+          acc += curr.amount
+        ), 0)}</span>
+      </h1>
     </>
   );
 }
