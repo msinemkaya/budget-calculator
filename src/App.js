2cfa8e43f581ba16d35a87bbf009d68d283169dd
Melike Sinem Kaya <msinemkaya6@gmail.com>
2023-05-05 21:40:47 +0300

    feat: added functionality of adding new inputs and displaying them

--- src/App.js
+++ src/App.js
@@ -1,30 +1,53 @@
-import './App.css'
-import ExpenseForm from './components/ExpenseForm'
-import ExpenseList from './components/ExpenseList'
-import Alert from './components/Alert'
-import {v4 as uuid} from 'uuid'
-import { useState } from 'react'
-
-const initialExpenses = [{
-  id: uuid(), charge: 'rent', amount: 666
-}]
+import './App.css';
+import ExpenseForm from './components/ExpenseForm';
+import ExpenseList from './components/ExpenseList';
+import Alert from './components/Alert';
+import { v4 as uuid } from 'uuid';
+import { useState } from 'react';
 
 function App() {
+  const [expenses, setExpenses] = useState([]);
+  const [amount, setAmount] = useState('');
+  const [charge, setCharge] = useState('');
 
-  const [expenses, setExpenses] = useState(initialExpenses)
+  const handleCharge = ({ target }) => {
+    setCharge(target.value);
+  };
+
+  const handleAmount = ({ target }) => {
+    setAmount(target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (charge !== '' && amount > 0) {
+      const singleExpense = { id: uuid(), charge, amount };
+      setExpenses([...expenses, singleExpense]);
+      setCharge('');
+      setAmount('');
+    }
+  };
 
   return (
     <>
       <Alert />
       <h1>budget calculator</h1>
       <main className='App'>
-        <ExpenseForm/>
-        <ExpenseList expenses={expenses}/>
+        <ExpenseForm
+          charge={charge}
+          amount={amount}
+          handleAmount={handleAmount}
+          handleCharge={handleCharge}
+          handleSubmit={handleSubmit}
+        />
+        <ExpenseList expenses={expenses} />
       </main>
       <h1>
-        Total Spending: <span className='total'>${expenses.reduce((acc, curr) => (
-          acc += curr.amount
-        ), 0)}</span>
+        Total Spending:{' '}
+        <span className='total'>
+          ${expenses.reduce((acc, curr) => (acc += parseInt(curr.amount)), 0)}
+        </span>
       </h1>
     </>
   );
--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,16 +1,38 @@
 import { MdSend } from 'react-icons/md';
 
-export default function ExpenseForm() {
+export default function ExpenseForm({
+  charge,
+  amount,
+  handleAmount,
+  handleCharge,
+  handleSubmit,
+}) {
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className='form-center'>
         <div className='form-group'>
           <label htmlFor='charge'>charge</label>
-          <input type='text' className='form-control' id='charge' name='charge' placeholder='e.g. rent'/>
+          <input
+            type='text'
+            className='form-control'
+            id='charge'
+            name='charge'
+            placeholder='e.g. rent'
+            value={charge}
+            onChange={handleCharge}
+          />
         </div>
         <div className='form-group'>
           <label htmlFor='amount'>amount</label>
-          <input type='text' className='form-control' id='amount' name='amount' placeholder='e.g. 600'/>
+          <input
+            type='number'
+            className='form-control'
+            id='amount'
+            name='amount'
+            placeholder='e.g. 600'
+            value={amount}
+            onChange={handleAmount}
+          />
         </div>
       </div>
       <button type='submit' className='btn'>
